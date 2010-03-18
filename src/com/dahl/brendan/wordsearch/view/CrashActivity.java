399a78efb83a1cda139f13d1a82555b225164ab6
Brendan Dahl <Brendan Dahl@localhost>
2010-03-18 19:47:07 -0400

    add analytics to crash activity to see rate of submissions

--- src/com/dahl/brendan/wordsearch/view/CrashActivity.java
+++ src/com/dahl/brendan/wordsearch/view/CrashActivity.java
@@ -1,6 +1,9 @@
 package com.dahl.brendan.wordsearch.view;
 
+import android.content.pm.PackageManager.NameNotFoundException;
+
 import com.firegnom.rat.ExceptionActivity;
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 public class CrashActivity extends ExceptionActivity {
 
@@ -11,6 +14,21 @@ public class CrashActivity extends ExceptionActivity {
 
 	@Override
 	public String getUrl() {
+		try {
+			GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
+			String appVer;
+			try {
+				appVer = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+			} catch (NameNotFoundException e) {
+				appVer = "unknown";
+			}
+			tracker.start("UA-146333-5", this);
+			tracker.trackPageView("/app/"+appVer+"/CrashActivity");
+			tracker.dispatch();
+			tracker.stop();
+		} catch (RuntimeException re) {
+		} catch (Exception e) {
+		}
 		return "http://wordsearchapp.brendandahl.com/app/crash";
 	}
 
