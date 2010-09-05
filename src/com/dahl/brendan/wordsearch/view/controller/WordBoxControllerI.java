42814d5f1255dc2c32bd7674418069119f893fb1
Brendan Dahl <dahl.brendan@gmail.com>
2010-09-04 22:33:38 -0400

    increase comments

--- src/com/dahl/brendan/wordsearch/view/controller/WordBoxControllerI.java
+++ src/com/dahl/brendan/wordsearch/view/controller/WordBoxControllerI.java
@@ -1,8 +1,32 @@
+//    This file is part of Open WordSearch.
+//
+//    Open WordSearch is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Open WordSearch is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Open WordSearch.  If not, see <http://www.gnu.org/licenses/>.
+//
+//	  Copyright 2009, 2010 Brendan Dahl <dahl.brendan@brendandahl.com>
+//	  	http://www.brendandahl.com
+
 package com.dahl.brendan.wordsearch.view.controller;
 
 import com.dahl.brendan.wordsearch.model.Grid;
 import com.dahl.brendan.wordsearch.model.Theme;
 
+/**
+ * handles all interactions that affect the word list part of the wordsearch activity
+ * 
+ * @author dahlb
+ *
+ */
 public interface WordBoxControllerI {
 	/**
 	 * 
@@ -17,7 +41,7 @@ public interface WordBoxControllerI {
 	 * @param str word to remove the list of words
 	 * @return number of words left to find
 	 */
-	public int wordFound(String str);
+	public void wordFound(String str);
 
 	/**
 	 * resets the list of words available to the user
@@ -26,6 +50,10 @@ public interface WordBoxControllerI {
 	 */
 	public void resetWords(Grid grid);
 
-	public int wordsLeft();	
+	/**
+	 * applies a theme to the implementation's views
+	 * 
+	 * @param theme new theme to apply
+	 */
 	public void updateTheme(Theme theme);
 }
